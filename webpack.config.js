9ddc3210b7b21064911eecdf79c45be07216b3ef
Pooja103 <pooja.leetcoder@gmail.com>
2026-03-24 23:48:51 +0530

    fix: build working

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-  },
+ output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'bundle.js',
+  clean: true,
+},
   module: {
     rules: [
       {
